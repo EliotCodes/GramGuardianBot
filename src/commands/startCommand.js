e7917f715a0cfcb07d021cb62be1852a17e8578d
Eliot Codes <eliotcodes@gmail.com>
2024-05-26 19:51:51 +0100

    FIX: Corrected the help Inline Keyboard

--- src/commands/startCommand.js
+++ src/commands/startCommand.js
@@ -1,7 +1,7 @@
 const { InlineKeyboard } = require("grammy");
 
 const startCommand = async (ctx) => {
-  const { first_name:firstName } = ctx.from;
+  const { first_name: firstName } = ctx.from;
   const { first_name: botName, username: botUsername } = ctx.me;
 
   const inlineKeyboard = new InlineKeyboard()
@@ -10,7 +10,7 @@ const startCommand = async (ctx) => {
     .url("👤 Developer", "https://t.me/eliotcodes")
     .url("🔗 Source Code", "https://github.com/eliotcodes/gramGuardianBot")
     .row()
-    .text("📖 Help", "/help");
+    .text("📖 Help", "help");
 
   await ctx.reply(
     `<b>Hello!</b> ${firstName} \n\nI am ${botName}, I can help you moderate your Telegram groups. \n\nIf you want to know more about me, please type /help`,
