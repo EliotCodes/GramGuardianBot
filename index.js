1afbf3077bd5174eacadd4372aa59ae7ef927079
Eliot Codes <eliotcodes@gmail.com>
2024-05-26 15:16:27 +0100

    Added the /start command and a To-do list

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+require("dotenv").config();
 const { Bot } = require("grammy");
 
 const bot = new Bot(process.env.BOT_TOKEN);
+
+// Commands
+
+bot.command("start", require("./src/commands/startCommand"));
+
+bot.start()
+console.log("Bot has started")
+
